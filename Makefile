960fb64058b51381a3b70449e45d29cfc0adea11
j-brady <jacob.brady@protonmail.com>
2021-11-27 20:21:28 -0500

    first draft

---
+++ Makefile
@@ -0,0 +1,19 @@
+# Makefile for mywebpage
+
+.PHONY: all publish publish_no_init
+
+all: publish
+
+publish: publish.el
+	@echo "Publishing... with current Emacs configurations."
+	emacs --batch --load publish.el --funcall org-publish-all
+
+publish_no_init: publish.el
+	@echo "Publishing... with --no-init."
+	emacs --batch --no-init --load publish.el --funcall org-publish-all
+
+clean:
+	@echo "Cleaning up.."
+	@rm -rvf *.elc
+	@rm -rvf html
+	@rm -rvf ~/.org-timestamps/*
---
+++ publish.el
@@ -0,0 +1,39 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(
+        ("j-brady.github.io-content"
+        :base-directory "./"
+        :base-extension "org"
+        :publishing-directory "./html/"
+        :recursive t
+        :publishing-function org-html-publish-to-html
+        :headline-levels 4
+        ;:auto-preamble t
+        :auto-sitemap t                ; Generate sitemap.org automagically...
+        :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+        :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+        :email "jacob.brady@protonmail.com"
+        :author "Jacob P Brady"
+        ; HTML5
+        :html-doctype "html5"
+        :html-html5-fancy t
+        ; Disable some Org's HTML defaults
+        :html-head-include-scripts nil
+        :html-head-include-default-style nil
+        ; HTML preamble
+        ;:html-preamble
+        :html-postamble "<p class=\"author\">Author: %a (%e)</p><p class=\"creator\">%c</p><div id=\"updated\">Updated: %C</div>"
+        )
+       ("j-brady.github.io-static"
+        :base-directory "/src/j-brady.github.io/"
+        :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+        :publishing-directory "~/src/j-brady.github.io/html/"
+        :recursive t
+        :publishing-function org-publish-attachment
+        )
+       ("j-brady.github.io"
+        :components ("j-brady.github.io-content" "j-brady.github.io-static"))))
+
+(provide 'publish)
+;;; publish.el
